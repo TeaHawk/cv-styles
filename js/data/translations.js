a3552a0fc9c76bf00b19f0451c5523372d002af4
TeaHawk <71761441+TeaHawk@users.noreply.github.com>
2025-02-13 12:38:00 +0100

    Create translations.js

---
+++ js/data/translations.js
@@ -0,0 +1,81 @@
+// === TRANSLATIONS MANAGER ===
+const translations = {
+    en: {
+        meta: {
+            language: "English",
+            direction: "ltr"
+        },
+        header: {
+            title: "Data Engineer",
+            contact: {
+                email: "📧 clauzel.daniel@gmail.com",
+                phone: "📱 +33 6 48 87 03 20"
+            }
+        },
+        controls: {
+            print: "Print / PDF",
+            theme: {
+                light: "Light Mode",
+                dark: "Dark Mode"
+            }
+        },
+        sections: {
+            experience: "Professional Experience",
+            skills: "Technical Skills",
+            education: "Education",
+			technicalBackground: "Technical Background"
+        }
+    },
+    fr: {
+        meta: {
+            language: "Français",
+            direction: "ltr"
+        },
+        header: {
+            title: "Ingénieur Data",
+            contact: {
+                email: "📧 clauzel.daniel@gmail.com",
+                phone: "📱 +33 6 48 87 03 20"
+            }
+        },
+        controls: {
+            print: "Imprimer / PDF",
+            theme: {
+                light: "Mode Clair",
+                dark: "Mode Sombre"
+            }
+        },
+        sections: {
+            experience: "Expérience Professionnelle",
+            skills: "Compétences Techniques",
+            education: "Formation",
+			technicalBackground: "Environnement Technique"
+        }
+    },
+    ru: {
+        meta: {
+            language: "Русский",
+            direction: "ltr"
+        },
+        header: {
+            title: "Инженер Данных",
+            contact: {
+                email: "📧 clauzel.daniel@gmail.com",
+                phone: "📱 +33 6 48 87 03 20"
+            }
+        },
+        controls: {
+            print: "Печать / PDF",
+            theme: {
+                light: "Светлая тема",
+                dark: "Тёмная тема"
+            }
+        },
+        sections: {
+            experience: "Профессиональный опыт",
+            skills: "Технические навыки",
+            education: "Образование",
+			technicalBackground: "Технический Стек"
+        }
+    }
+};
